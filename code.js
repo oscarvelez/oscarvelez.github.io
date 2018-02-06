89694c8bf118e1d3bf5ea8f79c0237272fea42e7
Oscar <oscard_velez@hotmail.com>
2018-02-06 16:02:05 -0700

    3 functions done

--- code.js
+++ code.js
@@ -2,16 +2,58 @@
 // this is just a stub for a function you need to implement
 //
 function getStats(txt) {
+
+
+
     return {
-        nChars: 123,
-        nWords: 22,
-        nLines: 10,
-        nNonEmptyLines: 22,
+        nChars: getnChars(txt),
+        nWords: getnWords(txt),
+        nLines: getnLines(txt),
+        nNonEmptyLines: getnNonEmptyLines(txt),
         averageWordLength: 3.3,
         maxLineLength: 33,
         palindromes: ["12321", "kayak", "mom"],
-        longestWords: ["xxxxxxxxx", "Pneumonoultramicroscopicsilicovolcanoconiosis"],
+        longestWords: ["xxxxxxxxx", "123444444"],
         mostFrequentWords: ["hello(7)", "world(1)"]
     };
 }
 
+function getnChars(txt) {
+    return txt.length;
+}
+
+function getnWords(txt) {
+    var words = txt.split(/\W+/);
+    return words.length;
+
+}
+function getnLines(txt){
+    if (txt !== '') {
+        var lines = txt.split(/\r\n|\r|\n/);
+        return lines.length;
+    }
+    else {
+        return 0;
+    }
+
+}
+
+function getnNonEmptyLines(txt) {
+
+}
+
+function getaverageWordLength(txt){
+
+}
+
+function getmaxLineLength(txt) {
+
+}
+
+function getPalindromes(txt) {
+
+}
+
+function getFrequentWords(txt){
+    
+}
